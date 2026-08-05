c989398ef166e800f70cae8a1ec7cebd8409d20c
Priyansh Verma <priyansh.verma1010@gmail.com>
2026-08-06 02:32:38 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+
+  static Set<String> commands;
+
   public static void main(String[] args) throws Exception {
+    commands = new HashSet<>();
+    commands.addAll(Arrays.asList("echo", "exit", "type"));
+
     Scanner scanner = new Scanner(System.in);
     while (true) {
       System.out.print("$ ");
@@ -11,6 +20,13 @@ public class Main {
         break;
       } else if (input.startsWith("echo ")) {
         System.out.println(input.substring(input.indexOf(' ') + 1));
+      } else if (input.startsWith("type ")) {
+        String command = input.substring(input.indexOf(' ') + 1);
+        if (isValidCommand(command)) {
+          System.out.println(command + " is a shell builtin");
+        } else {
+          System.out.println(command + ": not found");
+        }
       } else if (!isValidCommand(input)) {
         System.out.println(input + ": command not found");
       }
@@ -18,6 +34,6 @@ public class Main {
   }
 
   private static boolean isValidCommand(String command) {
-    return false;
+    return commands.contains(command);
   }
 }
