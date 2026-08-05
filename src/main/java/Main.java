1d0562a523130e35b9382de8a7fa5b3910dfc97d
Priyansh Verma <priyansh.verma1010@gmail.com>
2026-08-06 02:03:42 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
+  public static void main(String[] args) throws Exception {
+    System.out.print("$ ");
+    Scanner scanner = new Scanner(System.in);
+
+    String input = scanner.next();
+    if(!isValidCommand(input)) {
+      System.out.println(input + ": command not found");
     }
+  }
+
+  private static boolean isValidCommand(String command) {
+      return false;
+  }
 }
