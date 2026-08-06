bb77c08ddf067213f033b26cbfb2d6d2cf9dbd3c
Priyansh Verma <priyansh.verma1010@gmail.com>
2026-08-07 03:14:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -28,18 +25,15 @@ public class Main {
       } else if (command.equals("echo")) {
         System.out.println(remaining);
       } else if (command.equals("type")) {
-        if (isShellCommand(remaining)) {
-          System.out.println(remaining + " is a shell builtin");
-        } else {
-          String commandPath = findExecutable(remaining);
-          if (commandPath.isEmpty()) {
-            System.out.println(remaining + ": not found");
-          } else {
-            System.out.println(remaining + " is " + commandPath);
-          }
+        System.out.println(typeCommand(remaining));
+      } else {
+        Optional<Path> executable = findExecutable(command);
+        if(executable.isPresent()) {
+          runCommand(executable.get(), input);
         }
-      } else if (!isShellCommand(input)) {
-        System.out.println(input + ": command not found");
+        else {
+          System.out.println(command + ": command not found");
+        }
       }
     }
   }
@@ -48,16 +42,37 @@ public class Main {
     return shellCommands.contains(command);
   }
 
-  private static String findExecutable(String command) {
+  private static Optional<Path> findExecutable(String command) {
     String[] dirs = System.getenv("PATH").split(":");
-
     for (String dir : dirs) {
       Path candidate = Paths.get(dir, command);
       if (Files.isExecutable(candidate)) {
-        return candidate.toString();
+        return Optional.of(candidate);
       }
     }
 
-    return "";
+    return Optional.empty();
+  }
+
+  private static String typeCommand(String command) {
+    if (isShellCommand(command)) {
+      return command + " is a shell builtin";
+    }
+
+    Optional<Path> path = findExecutable(command);
+    return path.map(value -> command + " is " + value).orElseGet(() -> command + ": not found");
+  }
+
+  private static void runCommand(Path executable, String input) {
+    String[] parts = input.split(" ");
+    parts[0] = executable.toString();
+    ProcessBuilder processBuilder = new ProcessBuilder(parts);
+    processBuilder.inheritIO();
+
+    try (Process process = processBuilder.start()) {
+      process.waitFor();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 }
